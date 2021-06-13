a95d61f24165220e9eff4b51073b7eb0a0c12d8e
kwanmyeong lee <930614lkm@gmail.com>
2021-06-13 19:00:54 +0900

    User db

---
+++ LYL/src/src/myuser/MyuserDAO.java
@@ -0,0 +1,5 @@
+package src.myuser;
+
+public class MyuserDAO {
+
+}
---
+++ LYL/src/src/myuser/MyuserVO.java
@@ -0,0 +1,105 @@
+package src.myuser;
+
+import java.sql.Timestamp;
+
+public class MyuserVO {
+
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private int userPhone;
+	private String userEmail;
+	private Timestamp userJoin;
+	private int userSub;
+
+	public MyuserVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "MyuserVO [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
+				+ ", userPhone=" + userPhone + ", userEmail=" + userEmail + ", userJoin=" + userJoin + ", userSub="
+				+ userSub + "]";
+	}
+
+	public MyuserVO(int userNo, String userId, String userPwd, String userName, int userPhone, String userEmail,
+			Timestamp userJoin, int userSub) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.userPhone = userPhone;
+		this.userEmail = userEmail;
+		this.userJoin = userJoin;
+		this.userSub = userSub;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(int userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public Timestamp getUserJoin() {
+		return userJoin;
+	}
+
+	public void setUserJoin(Timestamp userJoin) {
+		this.userJoin = userJoin;
+	}
+
+	public int getUserSub() {
+		return userSub;
+	}
+
+	public void setUserSub(int userSub) {
+		this.userSub = userSub;
+	}
+
+}
