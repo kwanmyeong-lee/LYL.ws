3b0437834fa780e6a8b3c02f7e1f7185f7e19b50
이준경 <away0419@naver.com>
2021-06-22 18:11:18 +0900

    비밀번호

--- video.sql
+++ video.sql
@@ -430,7 +430,7 @@ ALTER TABLE HINT
         
 --유저 정보--        
 insert into myuser values(myuser_seq.nextval,'admin','admin','admin',01000000000,'lyl@gmail.com',default,default,null,default,null, default );
-insert into myuser values(myuser_seq.nextval,'aaa','aaa','aaa',01000000001,'aaa@gmail.com',default,default,null,default,null, default );
+insert into myuser values(myuser_seq.nextval,'aaa','aaa','aaa',01000000001,'aaa@naver.com',default,default,null,default,null, default );
 insert into myuser values(myuser_seq.nextval,'bbb','bbb','bbb',01000000002,'bbb@gmail.com',default,default,null,default,null, default );
 --유저 정보--
 
